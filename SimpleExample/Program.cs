90bb5fade4a08e2fe75c01e566c7846cdd04414e
MichelMichels <michel@miche.ls>
2022-07-15 13:38:13 +0200

    Initial project

---
+++ SimpleExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using SimpleExample;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+string typeName = configuration["messageWriter"];
+Type type = Type.GetType(typeName, throwOnError: true);
+
+IMessageWriter writer = (IMessageWriter)Activator.CreateInstance(type);
+var salutation = new Salutation(writer);
+salutation.Exclaim();
+
+Console.ReadLine();
---
+++ SimpleExample/Salutation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleExample
+{
+    public class Salutation
+    {
+        private readonly IMessageWriter _writer;
+
+        public Salutation(IMessageWriter writer)
+        {
+            if(writer == null)
+            {
+                throw new NullReferenceException($"Argument 'writer' is null");
+            }
+
+            _writer = writer;
+        }
+
+        public void Exclaim()
+        {
+            _writer.Write("Hello DI!");
+        }
+    }
+}
