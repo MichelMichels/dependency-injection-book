63c209052b4290bfdf5397d4c666fcd9ce9d64d8
MichelMichels <michel@miche.ls>
2022-09-19 09:05:01 +0200

    Added service to DI container

--- LooselyCoupled.UI/Program.cs
+++ LooselyCoupled.UI/Program.cs
@@ -1,7 +1,10 @@
+﻿using LooselyCoupled.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
